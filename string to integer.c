c360f310ba525e3c86030247a31b25c144983b1a
Gopala krishnan H <gopalakrishnan.hari95@gmail.com>
2016-06-28 11:46:26 +0530

    Create string to integer.c

---
+++ string to integer.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+void main()
+{
+int a;
+char b[5];
+clrscr();
+printf("\nEnter the value");
+scanf("%s",b);
+a=atoi(b);
+printf("\nthe integer value is %d",a);
+getch();
+}

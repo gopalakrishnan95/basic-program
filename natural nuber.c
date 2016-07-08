5b4eda22a188da74e1d96ff3c2197bf5575cc91b
Gopala krishnan H <gopalakrishnan.hari95@gmail.com>
2016-07-08 08:59:45 +0530

    Create natural nuber.c

---
+++ natural nuber.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,a;
+clrscr();
+printf("\n enter the natural numbers");
+scanf("%d",&n);
+a=(n*(n+1))/2;
+printf("\n sum of natural numbers %d",a);
+getch();
+}
